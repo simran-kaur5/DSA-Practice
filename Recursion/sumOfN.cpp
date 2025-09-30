d40b8f39aea51fd528cab8b39f2f0929251671d7
Simranjit Kaur <amarjitsohal769@gmail.com>
2025-09-30 19:03:44 +0530

    Added a file

---
+++ Recursion/sumOfN.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int sum(int n){
+    if(n==1){
+        return 1;
+    }
+    return n+sum(n-1);
+}
+int main(){
+    int n =5;
+    cout<<sum(n);
+    return 0;
+}
