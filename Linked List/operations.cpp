159619c9a2b84bed0cdcd09bbcd1bd141a511c43
Simranjit Kaur <amarjitsohal769@gmail.com>
2025-10-12 11:11:36 +0530

    Added content in file

--- Linked List/operations.cpp
+++ Linked List/operations.cpp
@@ -20,6 +20,8 @@ public:
         tail = NULL;
     }
 
+
+    //add node at front of LL
     void push_front(int val){
         Node* newNode = new Node(val);
         
@@ -31,7 +33,62 @@ public:
             head = newNode;
         }
     }
-    void print(){
+
+
+    //add node at last
+    void push_back(int val){
+        Node* newNode = new Node(val);   //created node dynamically
+
+        if(head == NULL){
+            head = tail = newNode;
+        }else{
+            Node* temp = head;
+            while(temp->next!=NULL){
+                temp = temp->next;
+            }
+            temp -> next = newNode;
+            newNode->next = NULL;
+        }
+    }
+
+    //delete node from front
+    void pop_front(){
+        if(head==NULL){
+            cout<<"Nothing to pop";
+            return;
+        }
+        if(head->next == NULL){
+            delete head; 
+        }else{
+            Node* temp = head;
+            head = head -> next;
+            temp->next = NULL;
+            delete temp;
+        }
+    }
+    
+
+    //delete Node from back
+    void pop_back(){
+        if(head == NULL){
+            cout<<"Nothing to delete";
+            return;
+        }
+
+        if(head->next==NULL){
+            delete head;
+        }else{
+            Node* temp = head;
+            while(temp->next->next!=NULL){
+                temp = temp->next;
+            }
+            delete temp -> next;  //create node dyanmically and also deleted dynamically
+            temp->next = NULL;
+        }
+
+    }
+    //print Full linked List
+     void print(){
         Node* temp = head;
 
         while(temp!= NULL){
@@ -47,6 +104,12 @@ int main(){
     l1.push_front(3);
     l1.push_front(2);
     l1.push_front(1);
+    l1.print();
+    l1.push_back(4);
+    l1.print();
+    l1.pop_front();
+    l1.print();
+    l1.pop_back();
     l1.print();
     
     return 0;
