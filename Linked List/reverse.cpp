e2d3a79f3e34e285bb045f5956c70e6fd7c2dea3
Simranjit Kaur <amarjitsohal769@gmail.com>
2025-10-13 21:20:56 +0530

    Added a file

---
+++ Linked List/reverse.cpp
@@ -0,0 +1,74 @@
+#include<iostream>
+using namespace std;
+class Node{   
+    public:
+    int data;
+    Node* next;
+    Node(int val){
+        data = val;
+        next = NULL;   
+    }
+};
+
+class list{
+    public:
+    Node* head;
+    Node* tail;
+
+    list(){
+        head = NULL;
+        tail = NULL;
+    }
+
+    void push_front(int val){
+        Node* newNode = new Node(val);
+        
+        //if linked list is empty
+        if(head== NULL){
+            head = tail = newNode;
+        }else{
+            newNode -> next = head;
+            head = newNode;
+        }
+    }
+
+     void print(){
+        Node* temp = head;
+
+        while(temp!= NULL){
+            cout<<temp -> data <<" ";
+            temp = temp->next;
+        }
+        cout<<endl;
+    }
+
+    void reverse(){
+        Node* temp = head;
+
+        Node* curr = head;
+        Node* prev = NULL;
+        Node* next = NULL;
+
+        while(curr!=NULL){
+            next = curr->next;
+
+            curr->next = prev;
+
+            prev =curr;
+            curr = next;
+        }
+        head = prev;
+    }
+};
+int main(){   
+
+    list l1;
+    l1.push_front(3);
+    l1.push_front(2);
+    l1.push_front(1);
+    l1.print();
+    l1.reverse();
+    l1.print();
+
+    return 0;
+}
