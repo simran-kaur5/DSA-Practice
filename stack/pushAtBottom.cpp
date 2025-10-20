fcc93e6ddb1aaeefe64e90b62ba02cf22053a3f9
Simranjit Kaur <amarjitsohal769@gmail.com>
2025-10-20 18:12:23 +0530

    Added a file

---
+++ stack/pushAtBottom.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+void pushBottom(stack<int>&s , int val){
+    if(s.size()==0){
+        s.push(val);
+        return;
+    }
+        int temp = s.top();   //save top value of stack to push it again
+        s.pop();   //pop the top
+        pushBottom(s,val);
+        s.push(temp);   //while bactracking push the popped value
+}
+int main(){
+
+    stack<int>s;
+
+    s.push(3);
+    s.push(2);
+    s.push(1);
+    int val = 4;
+
+    pushBottom(s,val);
+    while(!s.empty()){
+        cout<<s.top()<<" ";
+        s.pop();
+    }
+
+
+    return 0;
+}
