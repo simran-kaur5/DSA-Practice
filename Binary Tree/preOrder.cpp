239ef3888f5f53a041a5e7506069708082b72797
Simranjit Kaur <amarjitsohal769@gmail.com>
2025-10-27 08:53:09 +0530

    Added a file

---
+++ Binary Tree/preOrder.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Node{   //create individual nodes
+    public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val){
+        data = val;
+        left=right=NULL;
+    }
+
+};
+
+static int idx =-1; // we declared it as static because we want it incremented at each level of recursion
+Node* buildTree(vector<int>&nodes){
+    idx++; 
+    if(nodes[idx]==-1){
+        return NULL;
+    }
+
+    Node* currNode = new Node(nodes[idx]);
+    currNode->left = buildTree(nodes);
+    currNode->right = buildTree(nodes);
+
+    return currNode;
+
+}
+
+void preOrder(Node* root){
+    if(root==NULL){
+        return;
+    }
+
+    cout<<root->data<<" ";
+
+    preOrder(root->left);
+    preOrder(root->right);
+}
+int main(){
+
+    vector<int>nodes = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+
+    Node * root = buildTree(nodes);
+
+    preOrder(root);
+    return 0;
+}
