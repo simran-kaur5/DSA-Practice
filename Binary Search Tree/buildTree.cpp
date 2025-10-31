0540e394714b8064858ba0cc25cb2075646d34f0
Simranjit Kaur <amarjitsohal769@gmail.com>
2025-10-31 12:44:27 +0530

    Added a file

---
+++ Binary Search Tree/buildTree.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val){
+        data = val;
+        left = right = NULL;
+    }
+};
+
+Node* insert(Node* root,int val){
+    if(root==NULL){
+        root = new Node(val);
+        return root;
+    }
+
+    if(root->data>val){
+        root->left = insert(root->left,val);
+    }else{
+        root -> right = insert(root->right,val);
+    }
+
+    return root;
+}
+
+Node* build(int arr[],int n){
+    Node* root = NULL;
+
+    for(int i =0;i<n;i++){
+        root = insert(root,arr[i]);
+    }
+    return root;
+}
+
+void print(Node* root){
+    if(root==NULL){
+        return;
+    }
+    print(root->left);
+    cout<<root->data<<" ";
+    print(root->right);
+}
+int main(){
+    int arr[]= {5,2,3,7,10,6};
+    int n =6;
+    Node* root = build(arr,n);
+    print(root);
+    return 0;
+}
