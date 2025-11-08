15c60dd2109b17f66042b8b27f6670c65d7b713c
Simranjit Kaur <amarjitsohal769@gmail.com>
2025-11-08 17:59:56 +0530

    Added a file

---
+++ priorityQueue/classObject.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<string>
+#include<queue>
+using namespace std;
+class student{
+    public:
+    int marks;
+    string name;
+
+    student(string name,int marks){
+        this->name = name;
+        this->marks = marks;
+    }
+
+    bool operator < (const student &obj) const{
+        return this->marks < obj.marks;
+    }
+};
+int main(){
+    priority_queue<student>pq;
+
+    pq.push(student("bhumika",90));
+    pq.push(student("anamika",87));
+    pq.push(student("varun",65));
+
+    while(!pq.empty()){
+        cout<<"Top: "<<pq.top().name<<" have marks: "<<pq.top().marks<<endl;
+        pq.pop();
+    }
+
+    return 0;
+}
