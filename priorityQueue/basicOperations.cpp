d807530f1843f5b813f0785f7a90f97b6be67bb5
Simranjit Kaur <amarjitsohal769@gmail.com>
2025-11-08 13:18:06 +0530

    Added a part of file

--- priorityQueue/basicOperations.cpp
+++ priorityQueue/basicOperations.cpp
@@ -1,46 +1,94 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
-class Heap{
-    vector<int>vec;
+class Heap
+{
+    vector<int> vec;
 
-    public:
-
-    void push(int val){
-        //first push value at last idx
+public:
+    void push(int val)
+    {
+        // first push value at last idx
         vec.push_back(val);
 
-        //correct the CBT to make it max Heap
+        // correct the CBT to make it max Heap
 
-        int childIdx = vec.size()-1;
-        int parIdx = (childIdx -1)/2;
+        int childIdx = vec.size() - 1;
+        int parIdx = (childIdx - 1) / 2;
 
-        while(parIdx>=0 && vec[childIdx] > vec[parIdx]){
-            swap(vec[childIdx],vec[parIdx]);
+        while (parIdx >= 0 && vec[childIdx] > vec[parIdx])
+        {
+            swap(vec[childIdx], vec[parIdx]);
 
             childIdx = parIdx;
-            parIdx = (childIdx-1)/2;
+            parIdx = (childIdx - 1) / 2;
         }
     }
 
-        void pop(){
-
+    void fixHeap(int idx)
+    {
+        if (idx >= vec.size())
+        {
+            return;
         }
 
-        int top(){
-            return vec[0];
+        int left = 2 * idx + 1;
+        int right = 2 * idx + 2;
+
+        int maxIdx = idx;
+        if (left < vec.size() && vec[left] > vec[maxIdx])
+        {
+            maxIdx = left;
+        }
+        if (right < vec.size() && vec[right] > vec[maxIdx])
+        {
+            maxIdx = right;
         }
 
-        bool isEmpty(){
-            return vec.size()==0;
+        swap(vec[idx], vec[maxIdx]);
+        if (maxIdx != idx)
+        {
+            fixHeap(maxIdx);
         }
+    }
+    void pop()
+    {
+        if (isEmpty())
+        {
+            return;
+        }
+
+        swap(vec[0], vec[vec.size() - 1]); // O(1)
+
+        vec.pop_back(); // delete last element -> O(1)
+
+        fixHeap(0);  //this is called heapify it takes O(log n)
+    }
+
+    int top()
+    {
+        if (vec.empty())
+        {
+            cout << "Heap is empty!" << endl;
+            return -1; // or throw exception
+        }
+        return vec[0];
+    }
+
+    bool isEmpty()
+    {
+        return vec.size() == 0;
+    }
 };
-int main(){
+int main()
+{
     Heap heap;
     heap.push(10);
     heap.push(34);
-    
-    cout<<"Top of heap: "<<heap.top()<<endl;
+
+    cout << "Top of heap: " << heap.top() << endl;
+    heap.pop();
+    cout << "Top of heap: " << heap.top() << endl;
 
     return 0;
 }
