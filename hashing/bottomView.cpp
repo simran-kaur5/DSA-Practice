1cf958db86fd5fe320eaa5ca995947d6aa7ce585
Simranjit Kaur <amarjitsohal769@gmail.com>
2025-11-16 13:43:09 +0530

    Added a file

---
+++ hashing/bottomView.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+#include<vector>
+#include<map>
+#include<queue>
+using namespace std;
+class Node{   //create individual nodes
+    public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val){
+        data = val;
+        left=right=NULL;
+    }
+
+};
+
+static int idx =-1; // we declared it as static because we want it incremented at each level of recursion
+Node* buildTree(vector<int>&nodes){
+    idx++; 
+    if(nodes[idx]==-1){
+        return NULL;
+    }
+
+    Node* currNode = new Node(nodes[idx]);
+    currNode->left = buildTree(nodes);
+    currNode->right = buildTree(nodes);
+
+    return currNode;
+
+}
+
+void bottomView(Node* root){
+    queue<pair<Node*,int>>q;
+
+    q.push({root,0});
+
+    map<int,int>m;
+    while(!q.empty()){
+        pair<Node*,int>p = q.front();
+        q.pop();
+        Node* currNode = p.first;
+        int currHD = p.second;
+
+        m[currHD] = currNode->data;
+
+        if(currNode->left){
+            q.push({currNode->left,currHD-1});
+        }
+        if(currNode->right){
+            q.push({currNode->right,currHD+1});
+        }
+    }
+
+    for(auto it:m){
+        cout<<it.second<<" ";
+    }
+
+}
+int main(){
+
+    vector<int>nodes = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+
+    Node * root = buildTree(nodes);
+
+    bottomView(root);
+
+    return 0;
+}
