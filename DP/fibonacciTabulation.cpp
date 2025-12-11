5b5bfbf6662345956e9636a9d4289ed4d96a954a
Simranjit Kaur <amarjitsohal769@gmail.com>
2025-12-11 17:20:29 +0530

    Added a file

---
+++ DP/fibonacciTabulation.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int fib(int n){
+    vector<int>f(n+1,0);
+    f[1]=1;
+
+    for(int i =2;i<=n;i++){
+        f[i]=f[i-1]+f[i-2];
+    }
+
+    return f[n];    
+}
+int main(){
+    int n=6;
+    cout<<fib(n);
+}
