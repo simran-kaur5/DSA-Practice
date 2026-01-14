789405c156fc3bf665b8e4745b9eb0f85b5d3300
Simranjit Kaur <amarjitsohal769@gmail.com>
2026-01-14 12:09:36 +0530

    Added a file

--- DP/matrixMultiplication.cpp
+++ DP/matrixMultiplication.cpp
@@ -23,14 +23,43 @@ int multiplymemo(vector<int>&nums,int i ,int j,vector<vector<int>>&dp){
 
     return dp[i][j]=ans;
 }
+
+int multiplyTab(vector<int>&nums){
+    int n = nums.size();
+    vector<vector<int>>dp(n,vector<int>(n,0));
+
+    for(int i=1;i<n;i++){//all diagonals must be 0 because i==j
+        dp[i][i]=0;
+    }
+
+
+    for(int len =2;len<n;len++){
+        for(int i=1;i<=n-len;i++){
+            int j = i+len-1;
+            dp[i][j]=INT_MAX;
+
+            //where to partition
+            for(int k=i;k<j;k++){
+                int cost1 = dp[i][k];
+                int cost2 = dp[k+1][j];
+
+                int currCost = cost1 + cost2 + nums[i-1]*nums[k]*nums[j];
+
+                dp[i][j]=min(currCost,dp[i][j]);
+            }
+        }
+    }
+
+    return dp[1][n-1];
+}
 int main(){
 
     vector<int>nums = {1,2,3,4};
     int n =nums.size();
 
-    vector<vector<int>>dp(n,vector<int>(n,-1));
+    // vector<vector<int>>dp(n,vector<int>(n,-1));
 
-    cout<<"Minimum number of multiplication operation needed are: "<<multiplymemo(nums,1,n-1,dp);
+    cout<<"Minimum number of multiplication operation needed are: "<<multiplyTab(nums);
     
     return 0;
 }
